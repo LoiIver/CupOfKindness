fa4c69c71b3b0f46a2b168777ae45627168ff469
LoiIver <li-sam@mail.ru>
2020-10-07 17:27:17 +0300

    dancing skeleton

---
+++ Assets/ShadowScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowScript : MonoBehaviour
+{
+    public GameObject myParent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(myParent.transform.position.x, 0, myParent.transform.position.z);
+    }
+}
